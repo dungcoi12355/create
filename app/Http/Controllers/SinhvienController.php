67f7272875f594cfcde2c000e8f8bf48ad5d6f5b
dungvu <dungcoi12355@gmail.com>
2022-10-17 22:01:24 +0700

    alo thay

---
+++ app/Http/Controllers/SinhvienController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Sinhvien;
+
+class SinhvienController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $sinhvien = Sinhvien::paginate(5);
+        return view('index',compact('sinhvien'))->with('i', (request()->input('page', 1) -1) *5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Sinhvien::create($request->all());
+        return redirect()->route('sinhvien.index')->with('thongbao', 'Thêm sinh viên mới thành công!!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Sinhvien $sinhvien)
+    {
+        return view('edit', compact('sinhvien'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Sinhvien $sinhvien)
+    {
+        $sinhvien->update($request->all());
+        return redirect()->route('sinhvien.index')->with('thongbao', 'Cập nhật sinh viên thành công!!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Sinhvien $sinhvien)
+    {
+        $sinhvien->delete();
+        return redirect()->route('sinhvien.index')->with('thongbao', 'Xóa sinh viên thành công!!');
+    }
+}
